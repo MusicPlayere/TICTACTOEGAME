d244a38682cf44a7fffe6b97842ac835300f98d0
Musicplayer <162043584+MusicPlayere@users.noreply.github.com>
2024-03-13 11:39:03 +0000

    gfgueiuei

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const container = document.querySelector('.container');
-const signupButton = document.querySelector('.signup-section header');
-const loginButton = document.querySelector('.login-section header');
+const container = document.getElementById('container');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
 
-loginButton.addEventListener('click', () => {
-    container.classList.add('active');
+registerBtn.addEventListener('click', () => {
+    container.classList.add("active");
 });
 
-signupButton.addEventListener('click', () => {
-    container.classList.remove('active');
+loginBtn.addEventListener('click', () => {
+    container.classList.remove("active");
 });
